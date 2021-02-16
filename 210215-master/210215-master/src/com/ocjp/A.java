b77142b17f198b671a2ee560c0d3372de92c7d37
kim-heetae <rlaxo96@naver.com>
2021-02-16 11:50:02 +0900

    sadf

---
+++ 210215-master/210215-master/src/com/ocjp/A.java
@@ -0,0 +1,13 @@
+package com.ocjp;
+
+public class A {
+
+	public static void main(String[] args) {
+		if (1 != 1) {
+			System.out.println("참");
+		}else {
+			System.out.println("거짓");
+		}
+	}
+
+}
---
+++ 210215-master/210215-master/src/com/ocjp/Test.java
@@ -0,0 +1,20 @@
+package com.ocjp;
+
+public class Test {
+
+	public static void main(String[] args) {
+		int x = 5;
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		if((x == 4) && !b2) {
+			System.out.println("1");
+			System.out.println("2");
+		}
+		if((b2 = true) && true)
+		{
+			
+		}
+	}
+
+}
