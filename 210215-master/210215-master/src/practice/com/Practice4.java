315f432a2b76cc28fd00c99410709bca20d992b6
kosmo_30 <kosmo_30@DESKTOP-30A9LJD>
2021-02-17 09:19:47 +0900

    학원 실험

--- 210215-master/210215-master/src/practice/com/Practice4.java
+++ 210215-master/210215-master/src/practice/com/Practice4.java
@@ -8,7 +8,7 @@ public class Practice4 {//등비수열 배열사용
 	int[] seq = new int[7];//수열을 담을 배열
 	int num;//입력받는 등비수열의 기준값
 	boolean err = true;
-	void inputN() {
+	void inputN() {//입력받기
 		Scanner sc = new Scanner(System.in);
 		do {
 			try {//예외처리
@@ -23,7 +23,7 @@ public class Practice4 {//등비수열 배열사용
 			}
 		}while(!err);
 	}
-	void sequence() {
+	void sequence() {//수열 처리
 		for(int i = 0; i < 7; i++) {
 			if(i == 0) {
 				seq[i] = 1;//수열의 초기값은 1
